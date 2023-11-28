79a3da77c0d95e93bbff1b2aec4daf130487c4ee
Harry Boyd <hboyd255@gmail.com>
2023-11-28 18:30:27 +0000

    Created error indicator class

---
+++ lib/errorIndicator/errorIndicator.h
@@ -0,0 +1,55 @@
+#ifndef ERROR_INDICATOR_H
+#define ERROR_INDICATOR_H
+
+#include <Arduino.h>
+
+#include "drive.h"
+#include "pixels.h"
+
+#define ERROR_INDICATOR_DELAY = 500;
+
+/**
+ * @brief The ErrorIndicator class represents an error indicator module.
+ *
+ * This class is responsible for setting up the error indicator,
+ * setting the pixels and drive pointers, and indicating when an error occurs.
+ */
+class ErrorIndicator {
+   public:
+    /**
+     * @brief Default constructor for the ErrorIndicator class.
+     */
+    ErrorIndicator(){};
+
+    /**
+     * @brief Sets up the error indicator.
+     */
+    void setup();
+
+    /**
+     * @brief Sets the pixels pointer.
+     *
+     * @param pixelsPtr A pointer to the Pixels object.
+     */
+    void setPixels(Pixels* pixelsPtr);
+
+    /**
+     * @brief Sets the drive pointer.
+     *
+     * @param drivePtr A pointer to the Drive object.
+     */
+    void setDrive(Drive* drivePtr);
+
+    /**
+     * @brief Indicates that an error has occurred.
+     *
+     * @param errorMessage The error message to be displayed to.
+     */
+    void errorOccurred(String errorMessage);
+
+   private:
+    Pixels* pixelsPtr = NULL;  // Pointer to the Pixels object.
+    Drive* drivePtr = NULL;    // Pointer to the Drive object.
+};
+
+#endif  // ERROR_INDICATOR_H
--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,10 @@
 
 #include <Arduino.h>
 
+#include "ble.h"
 #include "bumper.h"
 #include "drive.h"
+#include "errorIndicator.h"
 #include "infrared.h"
 #include "pixels.h"
 #include "systemInfo.h"
@@ -11,20 +13,25 @@
 
 Drive drive;
 
+BluetoothLowEnergy ble;
 Pixels pixels(PIXELS_DATA_PIN);
 Ultrasonic ultrasonic(ULTRASONIC_TRIGGER, ULTRASONIC_ECHO, ULTRASONIC_TIMEOUT);
 Infrared leftInfrared(LEFT_INFRARED_INDEX);
 Infrared rightInfrared(RIGHT_INFRARED_INDEX);
 Bumper bumper(BUMPER_SHIFT_REG_DATA, BUMPER_SHIFT_REG_LOAD,
               BUMPER_SHIFT_REG_CLOCK, BUMPER_INTERRUPT_PIN);
+
+ErrorIndicator errorIndicator;
+
 bool bumperUpdate = false;
 
-
-
 void setup() {
-    Serial.begin(115200);
+    Serial.begin(SERIAL_BAUD_RATE);
+
     drive.setup();
     pixels.setup();
+
+    ble.setup();
 
     leftInfrared.setup();
     rightInfrared.setup();
@@ -37,22 +44,36 @@ void setup() {
     // Use a lambda function to set the bumperUpdate flag when the bumper is
     // pressed or released.
     bumper.assignCallback([]() { bumperUpdate = true; });
+
+    errorIndicator.setup();
+    errorIndicator.setPixels(&pixels);
+    errorIndicator.setDrive(&drive);
+    // errorIndicator.setImmobilizer(&drive);
 }
 
 void loop() {
-    drive.checkEncoders();
+    Serial.println(millis());
 
-    if (bumperUpdate) {
-        bumperUpdate = false;
+    drive.setVelocity(100);
 
-        u_int8_t bumperData = bumper.read();
-        pixels.displayBumperHard(bumperData);
-
+    if (millis() > 5000) {
+        errorIndicator.errorOccurred("millis too big");
     }
 
-    unsigned int distance = ultrasonic.read();
+    //     ble.poll();
+    //
+    //     Serial.println("Hello World");
 
-
-
-
+    //     drive.checkEncoders();
+    //
+    //     if (bumperUpdate) {
+    //         bumperUpdate = false;
+    //
+    //         u_int8_t bumperData = bumper.read();
+    //         pixels.displayBumperHard(bumperData);
+    //
+    //     }
+    //
+    //     unsigned int distance = ultrasonic.read();
+    //
 }
