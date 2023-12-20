c1efd920a56b6e8a17050a8ae894abecbcac66dc
Harry Boyd <hboyd255@gmail.com>
2023-12-20 22:14:17 +0000

    documented ErrorIndicator class

--- lib/errorIndicator/errorIndicator.cpp
+++ lib/errorIndicator/errorIndicator.cpp
@@ -1,55 +1,129 @@
-
+/**
+ * @file errorIndicator.h
+ * @brief DDefinitions for the ErrorIndicator class, responsible for halting
+ * operations upon an error, and drawing the users attention to the serial
+ * monitor.
+ *
+ * @author Harry Boyd - hboyd255@gmail.com
+ * @date 2023-12-20
+ * @copyright Copyright (c) 2023
+ */
 
 #include "errorIndicator.h"
 
+// The period to wait while flashing the leds
 #define ERROR_INDICATOR_DELAY 500
 
-ErrorIndicator::ErrorIndicator(uint8_t ledPin) { this->_ledPin = ledPin; }
+/**
+ * @brief Construct a new Error Indicator object
+ *
+ * @param ledPin (uint8_t) The pin of the LED to flash upon error, typically
+ * the built in led.
+ * @param serialBaudRate (uint32_t) the baud rate of the serial monitor, so
+ * that the class can reopen the serial monitor if needed.
+ */
+ErrorIndicator::ErrorIndicator(uint8_t ledPin, uint32_t serialBaudRate)
+    : _ledPin(ledPin), _serialBaudRate(serialBaudRate) {}
 
+/**
+ * @brief Sets up the class by setting the led pin to output.
+ */
 void ErrorIndicator::setup() { pinMode(this->_ledPin, OUTPUT); }
 
+/**
+ * @brief Assigns a Pixels object to pointer the class, used for flashing
+ * the lights to grab the users attention.
+ *
+ * @param pixelsPtr (Pixels*) A pointer to a preexisting Pixels class.
+ */
 void ErrorIndicator::assignPixels(Pixels* pixelsPtr) {
-    this->pixelsPtr = pixelsPtr;
+    this->_pixelsPtr = pixelsPtr;
 }
-void ErrorIndicator::assignDrive(Drive* drivePtr) { this->drivePtr = drivePtr; }
 
+/**
+ * @brief Assigns a Pixels object pointer to the class, used for halting the
+ * motors when an error occurs.
+ *
+ * @param drivePtr (Drive*) A pointer to a preexisting Drive class.
+ */
+void ErrorIndicator::assignDrive(Drive* drivePtr) {
+    this->_drivePtr = drivePtr;
+}
+
+/**
+ * @brief Permanently stops the program, flashes the given LED, flashes the
+ * Pixels LEDs(if available), stops the motors(if available) and prints and
+ * error message to the serial monitor.
+ *
+ * This can only be escaped by reseting the program.
+ *
+ * @param errorMessage (String) The error message to display to the serial
+ * monitor.
+ */
 void ErrorIndicator::errorOccurred(String errorMessage) {
-    if (this->drivePtr != NULL) {
-        this->drivePtr->stop();
+    // If a pointer to the drive class has been given, halt the motors.
+    if (this->_drivePtr != NULL) {
+        this->_drivePtr->stop();
     }
 
+    // Incase serial communication has not been initialized.
+    Serial.begin(this->_serialBaudRate);
+
+    // A boolean representing if the serial motor is connected.
     bool serialAvailability = (bool)Serial;
+    // A boolean representing is last state of the serialAvailability boolean,
+    // used for detecting the point at which the serial monitor becomes
+    // available.
     bool oldSerialAvailability = serialAvailability;
 
-    bool pixelsAvailability = this->pixelsPtr != NULL;
+    // A boolean representing if the pixels class has been provided.
+    bool pixelsAvailability = this->_pixelsPtr != NULL;
 
-    Serial.begin(SERIAL_BAUD_RATE);
-
+    // A boolean used for flipping the LED on and off.
     bool ledToggle = false;
 
+    // This loop will not end until the program is reset.
     while (true) {
+        // If the pixels class has been provided and the serial monitor is
+        // unavailable.
         if (pixelsAvailability && !serialAvailability) {
-            this->pixelsPtr->clear();
-            for (int i = 0; i < 8; i++) {
-                this->pixelsPtr->setPixel((i * 2) + ledToggle, 255, 0, 0);
-            }
-            this->pixelsPtr->show();
+            // Clear the LEDs.
+            this->_pixelsPtr->clear();
 
+            // Get the number of LEDs that the Pixels class represents.
+            uint16_t ledCount = this->_pixelsPtr->getLedCount();
+
+            // Set every other pixel to bright red, offset by the ledToggle
+            // boolean.
+            for (int i = 0; i < (ledCount / 2); i++) {
+                this->_pixelsPtr->setPixel((i * 2) + ledToggle, 255, 0, 0);
+            }
+            // Show all the LEDs.
+            this->_pixelsPtr->show();
+
+            // If the pixels class has been provided and the serial monitor is
+            // available, then turn off all the Pixels.
         } else if (pixelsAvailability && serialAvailability) {
-            pixelsPtr->clear(true);
+            _pixelsPtr->clear(true);
         }
 
+        // if the Serial monitor has only became available since the last loop.
         if (serialAvailability && !oldSerialAvailability) {
+            // Display the error message to the serial monitor.
             Serial.println("Error Occurred!");
             Serial.println(errorMessage);
         }
 
-        digitalWrite(LED_BUILTIN, ledToggle);
+        // Toggle the LED, regardless of if the serial monitor is available.
+        digitalWrite(this->_ledPin, ledToggle);
 
+        // Wait for a predefined period.
         delay(ERROR_INDICATOR_DELAY);
 
+        // Toggle the ledToggle boolean.
         ledToggle = !ledToggle;
 
+        // Check if the serial monitor has became available.
         oldSerialAvailability = serialAvailability;
         serialAvailability = (bool)Serial;
     }
--- lib/errorIndicator/errorIndicator.h
+++ lib/errorIndicator/errorIndicator.h
@@ -1,23 +1,88 @@
 #ifndef ERROR_INDICATOR_H
 #define ERROR_INDICATOR_H
 
+/**
+ * @file errorIndicator.h
+ * @brief Declaration for the ErrorIndicator class, responsible for halting
+ * operations upon an error, and drawing the users attention to the serial
+ * monitor.
+ *
+ * @author Harry Boyd - hboyd255@gmail.com
+ * @date 2023-12-20
+ * @copyright Copyright (c) 2023
+ */
+
 #include <Arduino.h>
 
 #include "drive.h"
 #include "pixels.h"
 
+/**
+ * @brief ErrorIndicator Class, responsible for halting operations upon an
+ * error, and drawing the users attention to the serial monitor.
+ */
 class ErrorIndicator {
    public:
-    ErrorIndicator(uint8_t ledPin);
+    /**
+     * @brief Construct a new Error Indicator object
+     *
+     * @param ledPin (uint8_t) The pin of the LED to flash upon error, typically
+     * the built in led.
+     * @param serialBaudRate (uint32_t) the baud rate of the serial monitor, so
+     * that the class can reopen the serial monitor if needed.
+     */
+    ErrorIndicator(uint8_t ledPin, uint32_t serialBaudRate);
+
+    /**
+     * @brief Sets up the class.
+     */
     void setup();
+
+    /**
+     * @brief Assigns a Pixels object to pointer the class, used for flashing
+     * the lights to grab the users attention.
+     *
+     * @param pixelsPtr (Pixels*) A pointer to a preexisting Pixels class.
+     */
     void assignPixels(Pixels* pixelsPtr);
+
+    /**
+     * @brief Assigns a Pixels object pointer to the class, used for halting the
+     * motors when an error occurs.
+     *
+     * @param drivePtr (Drive*) A pointer to a preexisting Drive class.
+     */
     void assignDrive(Drive* drivePtr);
+
+    /**
+     * @brief Permanently stops the program, flashes the given LED, flashes the
+     * Pixels LEDs(if available), stops the motors(if available) and prints and
+     * error message to the serial monitor.
+     *
+     * This can only be escaped by reseting the program.
+     *
+     * @param errorMessage (String) The error message to display to the serial
+     * monitor.
+     */
     void errorOccurred(String errorMessage);
 
    private:
+    /**
+     * @brief The  pin of the LED to flash upon error.
+     */
     uint8_t _ledPin;
-    Pixels* pixelsPtr = NULL;
-    Drive* drivePtr = NULL;
+    /**
+     * @brief The baud rate of the serial monitor.
+     */
+    uint32_t _serialBaudRate;
+    /**
+     * @brief A pointer to a preexisting Pixels class.
+     */
+    Pixels* _pixelsPtr = NULL;
+    /**
+     * @brief  A pointer to a preexisting Drive class.
+     */
+    Drive* _drivePtr = NULL;
 };
 
 #endif  // ERROR_INDICATOR_H
