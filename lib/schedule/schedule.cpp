ca3449adfe22352a82b353ed2ce42c62d67a97c4
Harry Boyd <hboyd255@gmail.com>
2023-12-21 18:26:20 +0000

    Created PassiveSchedule Class

--- lib/schedule/schedule.cpp
+++ lib/schedule/schedule.cpp
@@ -1,7 +1,7 @@
 /**
  * @file schedule.cpp
- * @brief Definitions of the Schedule class, the class responsible for keeping
- * track of when to execute routine functions.
+ * @brief Definitions of the Schedule  and PassiveSchedule classes, the class
+ * responsible for keeping track of when to execute routine functions.
  *
  * @author Harry Boyd - hboyd255@gmail.com
  * @date 2023-12-20
@@ -48,4 +48,29 @@ void Schedule::poll() {
     // The reasons there is nothing to handle millis overflowing if because the
     // max value of millis is equivelent to 49 days, which is far greater than
     // needed.
+}
+
+/**
+ * @brief Construct a new Passive Schedule object
+ *
+ * @param period (uint32_t) The number of milliseconds to wait before
+ * calling allowing the function to run again.
+ */
+PassiveSchedule::PassiveSchedule(uint32_t period)
+    : _period(period), _nextExecutionTime(millis() + period) {}
+
+/**
+ * @brief Check if the enough time has passed since the last time the a
+ * function ran. if enough time has passed, set the timer to one period in
+ * the future and return true.
+ *
+ * @return (true) If enough time has passed.
+ * @return (false) If enough time has not passed.
+ */
+bool PassiveSchedule::isReadyToRun() {
+    if (millis() > _nextExecutionTime) {
+        this->_nextExecutionTime = millis() + this->_period;
+        return true;
+    }
+    return false;
 }
--- lib/schedule/schedule.h
+++ lib/schedule/schedule.h
@@ -1,7 +1,7 @@
 /**
  * @file schedule.h
- * @brief Declaration of the Schedule class, the class responsible for keeping
- * track of when to execute routine functions.
+ * @brief Declaration of the Schedule and PassiveSchedule classes, responsible
+ * for keeping track of when to execute routine functions.
  *
  * @author Harry Boyd - hboyd255@gmail.com
  * @date 2023-12-20
@@ -59,9 +59,8 @@ class Schedule {
     voidFuncPtr _periodicFunctionPtr;
 
     /**
-     * @brief The number of milliseconds to wait before calling teh function
+     * @brief The number of milliseconds to wait before calling the function
      * again
-     *
      */
     uint32_t _period;
 
@@ -70,6 +69,43 @@ class Schedule {
      *
      */
     uint32_t _lastExecutionTime = 0;
+};
+
+/**
+ * @brief PassiveSchedule Class, Used to keep track of the last time that an
+ * execution took place.
+ */
+class PassiveSchedule {
+   public:
+    /**
+     * @brief Construct a new Passive Schedule object
+     *
+     * @param period (uint32_t) The number of milliseconds to wait before
+     * calling allowing the function to run again.
+     */
+    PassiveSchedule(uint32_t period);
+
+    /**
+     * @brief Check if the enough time has passed since the last time the a
+     * function ran. if enough time has passed, set the timer to one period in
+     * the future and return true.
+     *
+     * @return (true) If enough time has passed.
+     * @return (false) If enough time has not passed.
+     */
+    bool isReadyToRun();
+
+   private:
+    /**
+     * @brief The number of milliseconds to wait before calling allowing the
+     * function to run again.
+     */
+    uint32_t _period;
+
+    /**
+     * @brief The time at which the function will be allowed to run again.
+     */
+    uint32_t _nextExecutionTime;
 };
 
 #endif  // SCHEDULE_H
