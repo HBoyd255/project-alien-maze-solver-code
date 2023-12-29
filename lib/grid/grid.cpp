cf4eef7d890b20654f2a672c20d89b412aa3724f
Harry Boyd <hboyd255@gmail.com>
2023-12-29 21:51:40 +0000

    Hold This

--- lib/grid/grid.cpp
+++ lib/grid/grid.cpp
@@ -2,52 +2,93 @@
 
 #include "grid.h"
 
+#include <Arduino.h>
+
+#include "angleAndPosition.h"
+#include "bluetoothLowEnergy.h"
+#include "obstacles.h"
+
 const String edgeOfMapCharacter = "B";
-const String obstacleCharacters[4] = {"0", "1", "2", "3"};
+const String obstacleCharacters[4] = {"░", "▒", "▓", "█"};
 const String originCharacter = "S";
 const String startingPositionCharacter = "X";
 
-Grid::Grid(/* args */) {}
+Grid::Grid(BluetoothLowEnergy* blePtr) : _blePtr(blePtr) {}
 
 uint8_t Grid::getSensorType(Position position) {
-    int16_t xIndex = this->_getIndexFromMillimeter(position.x);
-    int16_t yIndex = this->_getIndexFromMillimeter(position.y);
+    int16_t yIndex = this->_getYIndexFromMillimeter(position.y);
+    int16_t xIndex = this->_getXIndexFromMillimeter(position.x);
 
-    if ((xIndex < 0) || (yIndex < 0)) {
-        Serial.println("Not in grid");
+    if ((yIndex < 0) || (yIndex < 0)) {
+        Serial.println("not in grid");
         return -1;
     }
 
-    return this->_data[xIndex][yIndex];
+    return _getData(yIndex, xIndex);
 }
 
-void Grid::setObstacle(Obstacle obstacleToAdd) {
-    int16_t xIndex = this->_getIndexFromMillimeter(obstacleToAdd.position.x);
-    int16_t yIndex = this->_getIndexFromMillimeter(obstacleToAdd.position.y);
+void Grid::setObstacle(Obstacle obstacleToAdd, bool sendOverBLE) {
+    int16_t yIndex = this->_getYIndexFromMillimeter(obstacleToAdd.position.y);
+    int16_t xIndex = this->_getXIndexFromMillimeter(obstacleToAdd.position.x);
 
-    if ((xIndex < 0) || (yIndex < 0)) {
+    if ((yIndex < 0) || (yIndex < 0)) {
         Serial.println("Not in grid");
         return;
     }
 
-    this->_data[xIndex][yIndex] = obstacleToAdd.sensorType;
-}
+    this->_setData(yIndex, xIndex, obstacleToAdd.sensorType);
 
-void Grid::updateObstacle(Obstacle obstacle) {
-    uint8_t existingValue = this->getSensorType(obstacle.position);
-    uint8_t newValue = obstacle.sensorType;
+    if (sendOverBLE) {
+        uint16_t totalIndex = yIndex * GRID_DIMENSION + xIndex;
 
-    if (newValue > existingValue) {
-        this->setObstacle(obstacle);
+        this->_blePtr->sendGridChunk(obstacleToAdd.sensorType, totalIndex, 1);
     }
 }
 
+void Grid::updateObstacle(Obstacle obstacle, bool sendOverBLE) {
+    uint8_t existingValue = this->getSensorType(obstacle.position);
+    uint8_t newValue = obstacle.sensorType;
+
+    // TODO set this back
+    //  if (newValue > existingValue) {
+    if (1) {
+        this->setObstacle(obstacle, sendOverBLE);
+    }
+}
+
+void Grid::sendEntireGridOverBLE() {
+    uint8_t recordedCharacter = this->_getData(0, 0);
+    uint32_t startIndex = 0;
+    uint32_t count = 0;
+
+    for (uint32_t index = GRID_LOWER_INDEX; index < GRID_ITEM_COUNT; index++) {
+        uint16_t y = index / GRID_DIMENSION;
+        uint16_t x = index % GRID_DIMENSION;
+
+        uint8_t valueAtCurrentIndex = this->_getData(y, x);
+
+        if (valueAtCurrentIndex != recordedCharacter) {
+            count = index - startIndex;
+
+            this->_blePtr->sendGridChunk(recordedCharacter, startIndex, count);
+
+            startIndex = index;
+
+            recordedCharacter = valueAtCurrentIndex;
+        }
+    }
+
+    count = GRID_ITEM_COUNT - startIndex;
+
+    this->_blePtr->sendGridChunk(recordedCharacter, startIndex, count);
+}
+
 void Grid::print(Position centrePosition, uint8_t radius) {
-    int16_t xCentreIndex = this->_getIndexFromMillimeter(centrePosition.x);
-    int16_t yCentreIndex = this->_getIndexFromMillimeter(centrePosition.y);
+    int16_t xCentreIndex = this->_getXIndexFromMillimeter(centrePosition.x);
+    int16_t yCentreIndex = this->_getYIndexFromMillimeter(centrePosition.y);
 
     if ((xCentreIndex < 0) || (yCentreIndex < 0)) {
-        Serial.println("Not in grid");
+        Serial.println("Not In Grid");
         return;
     }
 
@@ -74,7 +115,7 @@ void Grid::print(Position centrePosition, uint8_t radius) {
         Serial.println();
     }
 
-    for (int16_t yIndex = yUpperIndex; yIndex >= yLowerIndex; yIndex--) {
+    for (int16_t yIndex = yLowerIndex; yIndex <= yUpperIndex; yIndex++) {
         if (xLowerIndex == GRID_LOWER_INDEX) {
             Serial.print(edgeOfMapCharacter);
         }
@@ -85,7 +126,8 @@ void Grid::print(Position centrePosition, uint8_t radius) {
             } else if ((xIndex == xCentreIndex) && (yIndex == yCentreIndex)) {
                 Serial.print(startingPositionCharacter);
             } else {
-                Serial.print(obstacleCharacters[this->_data[yIndex][xIndex]]);
+                Serial.print(
+                    obstacleCharacters[this->_getData(yIndex, xIndex)]);
             }
         }
 
@@ -111,6 +153,73 @@ void Grid::print(Position centrePosition, uint8_t radius) {
     Serial.print("\n");
 }
 
+#include "binary.h"
+void Grid::_setData(uint16_t yIndex, uint16_t xIndex, uint8_t value) {
+    // Look up table for the bitmasks to clear the data in the target position.
+    const uint8_t punchMasks[4] = {0b11111100, 0b11110011, 0b11001111,
+                                   0b00111111};
+
+    const uint8_t dataMasks[4][4] = {
+        {0b00000000, 0b00000001, 0b00000010, 0b00000011},
+        {0b00000000, 0b00000100, 0b00001000, 0b00001100},
+        {0b00000000, 0b00010000, 0b00100000, 0b00110000},
+        {0b00000000, 0b01000000, 0b10000000, 0b11000000}};
+
+    // DOing this little endian
+    uint16_t outerXIndex = xIndex;
+    uint16_t innerXIndex = outerXIndex >> 2;
+    uint16_t innerBlobIndex = outerXIndex & 3;
+
+    uint8_t blob = _data[yIndex][innerXIndex];
+
+    Serial.print(" xIndex:");
+    Serial.print(xIndex);
+    Serial.print(" yIndex:");
+    Serial.print(yIndex);
+    Serial.print(" innerXIndex:");
+    Serial.print(innerXIndex);
+    Serial.print(" innerBlobIndex:");
+    Serial.print(innerBlobIndex);
+
+    Serial.print(" Data :");
+    Serial.print(value);
+    Serial.println();
+
+    Serial.print(" blob:");
+    printByte(blob);
+
+    blob &= punchMasks[innerBlobIndex];
+    Serial.print(" Punch:");
+    printByte(punchMasks[innerBlobIndex]);
+
+    Serial.print(" blob:");
+    printByte(blob);
+
+    blob |= dataMasks[innerBlobIndex][value];
+    Serial.print(" dataMasks:");
+    printByte(dataMasks[innerBlobIndex][value]);
+
+    Serial.print(" blob:");
+    printByte(blob);
+
+    this->_data[yIndex][innerXIndex] = blob;
+
+    Serial.print(" Now To read :");
+    Serial.println(_getData(yIndex, xIndex));
+    Serial.println();
+}
+uint8_t Grid::_getData(uint16_t yIndex, uint16_t xIndex) {
+    uint16_t outerXIndex = xIndex;
+    uint16_t innerXIndex = outerXIndex >> 2;
+    uint16_t innerBlobIndex = outerXIndex & 3;
+
+    uint8_t blob = _data[yIndex][innerXIndex];
+
+    uint8_t value = blob >> (innerBlobIndex * 2) & 3;
+
+    return value;
+}
+
 int16_t Grid::_roundToNearestCentimeter(int16_t valueInMM) {
     // offset the value in mm so that it is rounded to the nearest value,
     // not
@@ -121,7 +230,19 @@ int16_t Grid::_roundToNearestCentimeter(int16_t valueInMM) {
     return valueInCM;
 }
 
-int16_t Grid::_getIndexFromMillimeter(int16_t distanceMM) {
+int16_t Grid::_getYIndexFromMillimeter(int16_t distanceMM) {
+    int16_t distanceCM = this->_roundToNearestCentimeter(distanceMM);
+
+    int16_t index = GRID_RADIUS_CM - distanceCM;
+
+    if ((index < GRID_LOWER_INDEX) || (index > GRID_UPPER_INDEX)) {
+        return -1;
+    }
+
+    return index;
+}
+
+int16_t Grid::_getXIndexFromMillimeter(int16_t distanceMM) {
     int16_t distanceCM = this->_roundToNearestCentimeter(distanceMM);
 
     int16_t index = GRID_RADIUS_CM + distanceCM;
