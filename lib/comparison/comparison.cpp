c275f5373f8e5d35713590f19b1d804d53f9c023
Harry Boyd <hboyd255@gmail.com>
2024-01-31 17:30:07 +0000

    documented comparison module

--- lib/comparison/comparison.cpp
+++ lib/comparison/comparison.cpp
@@ -2,6 +2,16 @@
 
 #include "comparison.h"
 
+/**
+ * @brief Checks wether the absolute difference between two given values is
+ * within a given range(inclusive).
+ *
+ * @param valueA The first integer to compare.
+ * @param valueB The second integer to compare
+ * @param range The inclusive range that the valses should be in of each other.
+ * @return (true) If abs(valueA - valueB) <= range.
+ * @return (false) If abs(valueA - valueB) > range.
+ */
 bool within(int valueA, int valueB, int range) {
     return abs(valueA - valueB) <= range;
 }
--- lib/comparison/comparison.h
+++ lib/comparison/comparison.h
@@ -3,6 +3,16 @@
 
 #include <Arduino.h>
 
+/**
+ * @brief Checks wether the absolute difference between two given values is
+ * within a given range(inclusive).
+ *
+ * @param valueA The first integer to compare.
+ * @param valueB The second integer to compare
+ * @param range The inclusive range that the valses should be in of each other.
+ * @return (true) If abs(valueA - valueB) <= range.
+ * @return (false) If abs(valueA - valueB) > range.
+ */
 bool within(int valueA, int valueB, int range);
 
 #endif  // COMPARISON_H
